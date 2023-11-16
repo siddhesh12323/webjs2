dd26e0c7584182b5d898f52a6482a285862eee8f
Siddhesh Kumbhar <purnakumbhar1234@gmail.com>
2023-11-17 00:54:55 +0530

    checked property and tried api

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 /*
+INTRO
 let num1 = 5;
 let num2 = 5/2;
 
@@ -9,12 +10,14 @@ console.log(num2)
 */
 
 /*
+WINDOW PROMPT
 let username;
 username = window.prompt("What's your name?");
 console.log(username)
 */
 
 /*
+FUNCTIONS
 let username;
 document.getElementById("myButton").onclick = function () {
     username = document.getElementById("myText").value
@@ -23,6 +26,7 @@ document.getElementById("myButton").onclick = function () {
 */
 
 /*
+CIRCUMFERENCE
 let circumference;
 document.getElementById("myButton").onclick = function () {
     circumference = Math.round(2*Math.PI*document.getElementById("myNumber").value);
@@ -31,6 +35,7 @@ document.getElementById("myButton").onclick = function () {
 */
 
 /*
+HYPOTENUSE
 let hypotenuse;
 document.getElementById("myButton").onclick = function () {
     hypotenuse = Math.round(Math.sqrt(Math.pow(document.getElementById("sideA").value, 2) + Math.pow(document.getElementById("sideB").value, 2)));
@@ -39,6 +44,7 @@ document.getElementById("myButton").onclick = function () {
 */
 
 /*
+COUNTER
 let count = 0;
 document.getElementById("increment").onclick = function () {
     count++;
@@ -54,6 +60,8 @@ document.getElementById("reset").onclick = function () {
 }
 */
 
+/*
+ROLL DICE, ADD ELEMENTS BY JS TO A DIV
 let numberOfSides;
 let numberOfDice;
 let output;
@@ -69,3 +77,78 @@ document.getElementById("roll").onclick = function () {
         document.getElementById("myDiv").appendChild(document.createElement("br"));
     }
 }
+*/
+
+/*
+// CHECKED
+let checked, upi, cod, emi;
+
+document.getElementById("submit").onclick = function () {
+    // Clear previous content in myDiv
+    document.getElementById("myDiv").innerHTML = "";
+
+    checked = document.getElementById("checkbox1");
+    upi = document.getElementById("upi");
+    cod = document.getElementById("cod");
+    emi = document.getElementById("emi");
+
+    if (checked.checked) {
+        console.log("Checked!");
+        addOutput("Checked!");
+    } else {
+        console.log("Not Checked :(");
+        addOutput("Not Checked :(");
+    }
+
+    if (upi.checked) {
+        console.log("UPI selected!");
+        addOutput("UPI selected!");
+    } else if (cod.checked) {
+        console.log("COD");
+        addOutput("COD selected!");
+    } else if (emi.checked) {
+        console.log("EMI");
+        addOutput("EMI selected!");
+    } else {
+        console.log("No payment selected :(");
+        addOutput("No payment selected :(");
+    }
+};
+
+function addOutput(text) {
+    let paragraph = document.createElement("p");
+    paragraph.textContent = text;
+    document.getElementById("myDiv").appendChild(paragraph);
+    document.getElementById("myDiv").appendChild(document.createElement("br"));
+}
+*/
+
+// Replace 'YOUR_API_KEY' with your actual API key
+        const apiKey = '026b3c07337543e29a2190611231611';
+
+        const apiUrl = 'http://api.weatherapi.com/v1/current.json';
+
+        // Make a GET request to the API with the API key in the headers
+        fetch(apiUrl, {
+            method: 'GET',
+            headers: {
+                'key': apiKey,
+                'q': "Paris",
+            }
+        })
+        .then(response => {
+            // Check if the request was successful (status code 200-299)
+            if (!response.ok) {
+                throw new Error(`HTTP error! Status: ${response.status}`);
+            }
+            // Parse the response as JSON
+            return response.json();
+        })
+        .then(data => {
+            // Handle the data from the API
+            console.log('API Response:', data);
+        })
+        .catch(error => {
+            // Handle any errors that occurred during the fetch
+            console.error('Fetch error:', error);
+        });
